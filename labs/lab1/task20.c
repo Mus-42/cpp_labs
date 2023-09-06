33c4c2ab5e75d9e3bc12eeb24736034c37089eb3
_Mus <favmake@gmail.com>
2023-09-06 16:50:53 +0300

    lab1 all extra tasks

---
+++ labs/lab1/task20.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// Lab 1 Task 20
+
+int main(void) {
+    float a, b, c;
+    // Input format template: "A=xxx.xxx, B=xxExxx C=xxx.xxxx"
+    scanf("A=%f, B=%E C=%f", &a, &b, &c);
+
+    float average = (a+b+c)/3.;
+    float average_harmonic = 3./(1./a + 1./b + 1./c);
+
+    printf("average = %.4f harmonic = %.4f\n", average, average_harmonic);
+    printf("average = %.4e harmonic = %.4e\n", average, average_harmonic);
+}
