89399d0fdecadf8ab50c33156ccd98ba8a748f59
_Mus <favmake@gmail.com>
2023-09-08 13:17:42 +0300

    lab7 implemented tasks 18, 20, code template for task 19

---
+++ labs/lab7/task19.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// Lab 7 Task 19
+
+// Compute poly from roots
+// coefficients - ouput buf1, buf2 - temp bufs (all of them at least roots_n+1 size)
+void compute_poly(int roots_n, const int* roots, int* coefficients, int* buf1, int* buf2) {
+    // TODO
+}
+
+int main(void) {
+    // TODO
+}
