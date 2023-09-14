dea2f753848798ccf53d793f7de3e024de3eb212
_Mus <favmake@gmail.com>
2023-09-14 20:41:14 +0300

    lab4 classwork 13.09

---
+++ labs/lab4/task2.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+// Lab 4 Task 2
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d! = 1", n);
+    for (int i = 2; i <= n; i++) {
+        printf("*%d", i);
+    }
+    printf("\n%d! = %d", n, n);
+    for (int i = n-1; i >= 1; i--) {
+        printf("*%d", i);
+    }
+    putchar('\n');
+}
+
---
+++ labs/lab4/task3.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+// Lab 4 Task 3 b
+
+double t4_a(int n, double x) {
+    double xpow = 1.;
+    double sum = 1.;
+
+    for (int i = 0; i < n; i++) {
+        xpow *= x;
+        sum += xpow;
+    }
+
+    return sum;
+}
+
+double t4_b(int n, double x, double y) {
+    double xpow = 1., ypow = 1.;
+    double sum = 1.;
+
+    double x2 = x*x;
+
+    for (int i = 0; i < n; i++) {
+        xpow *= x2;
+        ypow *= y;
+        sum += xpow * ypow;
+    }
+
+    return sum;
+}
+
+int main() {
+    // a)
+    printf("a) %lf\n", t4_a(3, 2.));
+    // b)
+    printf("b) %lf\n", t4_b(4, 1., 2.));
+}
+
