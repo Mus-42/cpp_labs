c2cdcad185505230f2cc1dd194fe23e2d2bd0e65
_Mus <favmake@gmail.com>
2023-10-05 19:41:54 +0300

    04.10 lab9 classwork

---
+++ labs/lab9/task1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+// Lab 9 Task 1
+
+int main(void) {
+    unsigned n;
+    scanf("%u", &n);
+    double* arr = (double*)malloc(n * sizeof(double));
+    
+    if (!arr) 
+        return EXIT_FAILURE;
+
+    for (unsigned i = 0; i < n; i++) {
+        scanf("%lf", arr+i);
+    }
+
+    double sqr_sum = 0.;
+    for (unsigned i = 0; i < n; i++)
+        sqr_sum += arr[i] * arr[i];
+
+    printf("%lf\n", sqr_sum);
+
+    free(arr);
+}
