71745c4b953cd9d8d35d6045b54a8d5bc9212a60
_Mus <favmake@gmail.com>
2023-11-20 18:32:00 +0200

    lab14 hw all

---
+++ labs/lab14/task16.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <iomanip>
+#include <charconv>
+#include <system_error>
+
+// Lab 14 task 16
+
+int main() {
+    freopen("data/lab14_task16.txt", "r", stdin);
+    freopen("data/lab14_task16_out.txt", "w", stdout);
+
+    std::string buf;
+    std::cout << std::setprecision(10);
+
+    while (!std::cin.eof()) {
+        std::cin >> buf;
+        double num{};
+        auto [_ptr, ec] = std::from_chars(buf.data(), buf.data()+buf.size(), num);
+        if (ec == std::errc::invalid_argument || num < 0.) {
+            std::cout << buf << ' ';
+        } else {
+            std::cout << std::log(num) << ' ';
+        }
+    }
+}
