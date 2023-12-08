410fe9469a48009390d659c7b1592cd91b5a7fbf
_Mus <favmake@gmail.com>
2023-12-08 11:31:44 +0200

    lab15 task 12 fixed

--- labs/lab15/task12.cpp
+++ labs/lab15/task12.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <concepts>
 
 // Lab 15 task 12
 
@@ -17,11 +18,11 @@ public:
         data = std::make_unique_for_overwrite<T[]>(other.len);
         std::copy(other.data.get(), other.data.get() + other.len, data.get());
     }
-    
+
     ~Vec() = default;
 
     template<size_t N>
-    Vec(const T (&elements)[N]) noexcept : len(N), data(std::make_unique_for_overwrite<T[]>(N)) {
+    Vec(const T(&elements)[N]) noexcept : len(N), data(std::make_unique_for_overwrite<T[]>(N)) {
         std::copy(elements, elements + N, data.get());
     }
 
@@ -29,23 +30,71 @@ public:
         return len;
     }
 
+    bool is_empty() const noexcept {
+        return len == 0;
+    }
+
     T& operator[](size_t at) noexcept {
         return data.get()[at];
     }
-    
+
     const T& operator[](size_t at) const noexcept {
         return data.get()[at];
+    }
+
+    T dot(const Vec& other) const {
+        if (len != other.len) {
+            throw std::invalid_argument("can't compute dot product of vectors with different size");
+        }
+        T v = 0;
+        for (size_t i = 0; i < len; i++) {
+            v += data.get()[i] * other.data.get()[i];
+        }
+        return v;
     }
 private:
     size_t len{};
     std::unique_ptr<T[]> data;
 };
 
-int main() {
-    Vec<int> q{{1,2,3,4,5,6,7}};
-    auto q2 = q;
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const Vec<T>& vec)
+    requires(requires(std::ostream& out, const T& v) {
+        { out << v } -> std::same_as<std::ostream&>;
+    })
+{
+    out << "[";
+    if (!vec.is_empty()) {
+        out << vec[0];
+        for (size_t i = 1; i < vec.size(); i++) {
+            out << ", " << vec[i];
 
-    for (size_t i = 0; i < q2.size(); i++) {
-        std::cout << q2[i] << ' ';
+        }
     }
+    out << "]";
+    return out;
+}
+
+template<typename T>
+std::istream& operator>>(std::istream& in, Vec<T>& vec)
+    requires(requires(std::istream& in, T& v) {
+        { in >> v } -> std::same_as<std::istream&>;
+    })
+{
+    for (size_t i = 0; i < vec.size(); i++) {
+        in << vec[i];
+    }
+    return in;
+}
+
+int main() {
+    const Vec<int> q{ {1,2,3,4,5,6,7} };
+    const auto q2 = q;
+
+    std::cout << q2 << std::endl; // prints [1, 2, 3, 4, 5, 6, 7] as expected
+
+    const Vec<int> v{ {1,0,0,1,0,0,-1} };
+    const auto dot = q.dot(v);
+
+    std::cout << dot << std::endl; // 1+4-7 = -2
 }
