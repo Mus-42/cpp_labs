08e6a070a6af8ce2799e8feb325449cd2242939a
_Mus <favmake@gmail.com>
2023-12-12 14:12:59 +0200

    lab18 hw

---
+++ labs/lab18/task3.cpp
@@ -0,0 +1,40 @@
+#include <memory>
+#include <string>
+#include <iostream>
+
+// in this implementation ve are wery lucky and "random" element from problem statement is always last element
+
+template<typename T>
+class BlackBox {
+public:
+    BlackBox() noexcept = default;
+
+    bool is_empty() const noexcept {
+        return m_root == nullptr;
+    }
+
+    void push(const T& el) {
+        m_storage.push_back(el);
+    }
+
+    void push(T&& el) {
+        m_storage.push_back(el);
+    }
+
+    void pop() {
+        m_storage.pop_back();
+    }
+
+    const T& xpop() const {
+        return m_root.back();
+    }
+private:
+    std::vector<T> m_storage;
+};
+
+int main() {
+    BlackBox<int> bb;
+    bb.push(42);
+
+    std::cout << bb.xpop() << std::endl;
+}
